44c33afa74e7a95a1a709d481c84d12476cb3e97
Yogesh K <armsvcmode@gmail.com>
2022-06-09 13:45:35 +0530

    Show different options if the current logged in user is a company, like a link to post a new job

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import prisma from 'lib/prisma'
-import { getJobs } from 'lib/data.js'
+import { getJobs,getUser } from 'lib/data.js'
 import Jobs from 'components/Jobs'
-import { useSession } from 'next-auth/react'
+import { useSession, getSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 // iterate on the jobs array and print the jobs details:
 // create a job component
-export default function Home({jobs}) {
+export default function Home({jobs, user}) {
   const { data: session, status } = useSession()
   const router = useRouter()
 
@@ -29,6 +29,40 @@ export default function Home({jobs}) {
         </Link>
       )}
 
+      {session && (
+        <>
+          <p className='mb-10 text-2xl font-normal'>
+            Welcome, {user.name}
+            {user.company && (
+              <span className='bg-black text-white uppercase text-sm p-2'>
+                Company
+              </span>
+            )}
+          </p>
+          {user.company ? (
+            <>
+              <button
+                className='border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+              >
+                click here to post a new job
+              </button>
+              <button
+                className='ml-5 border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+              >
+                see all the jobs you posted
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className='ml-5 border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+              >
+                see all the jobs you applied to
+              </button>
+            </>
+          )}
+        </>
+      )}
       <Jobs jobs={jobs}/>
     </div>
   )
@@ -36,11 +70,18 @@ export default function Home({jobs}) {
 
 
 export async function getServerSideProps(context) {
+    const session = await getSession(context)
+
     let jobs = await getJobs(prisma)
     jobs = JSON.parse(JSON.stringify(jobs))
+
+    let user = await getUser(session.user.id, prisma)
+    user = JSON.parse(JSON.stringify(user))
+
     return {
         props: {
           jobs,
+          user,
         },
     }
 }
