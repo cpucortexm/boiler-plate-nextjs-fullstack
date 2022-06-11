c95cbfcfeca3ea304763b5acea5fa9802337df32
Yogesh K <armsvcmode@gmail.com>
2022-06-11 22:26:10 +0530

    Once the user applied to a job, we’ll show “already applied” and won’t let adding new applications from them to this specific job

--- pages/job/[id].js
+++ pages/job/[id].js
@@ -1,9 +1,10 @@
 // This is a dynamic page with [id] being a dynamic place holder
-import { getJob } from 'lib/data'
+import { getJob , alreadyApplied} from 'lib/data'
 import prisma from 'lib/prisma'
 import Link from 'next/link'
+import { getSession } from 'next-auth/react'
 
-export default function Job({job}) {
+export default function Job({job, applied }) {
   
   return (
     <div className='flex flex-col w-1/2 mx-auto'>
@@ -23,13 +24,24 @@ export default function Job({job}) {
         <div className='mb-4 mt-20'>
                 <div className='pl-16 pr-16 -mt-6'>
                 <p className='text-base font-normal mt-3'>{job.description}</p>
-                <div className='mt-20 flex justify-center '>
-                    <Link href={`/job/${job.id}/apply`}>
-                        <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
-                        Apply to this job
-                        </button>
-                    </Link>
-                </div>
+                {/* check the applied state, and show a link to the dashboard instead*/}
+                {applied ? (
+                    <div className='mt-20 flex justify-center '>
+                        <Link href={`/dashboard`}>
+                            <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
+                                You already applied!
+                            </button>
+                        </Link>
+                  </div>
+                ) : (
+                    <div className='mt-20 flex justify-center '>
+                        <Link href={`/job/${job.id}/apply`}>
+                            <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
+                            Apply to this job
+                            </button>
+                        </Link>
+                    </div>
+                )}
                 <div className='mt-4'>
                     <h4 className='inline'>Posted by</h4>
                     <div className='inline'>
@@ -56,13 +68,22 @@ export default function Job({job}) {
 
 // we’ll get the job id in the getServerSideProps context object, under the params property,
 export async function getServerSideProps(context) {
+    const session = await getSession(context)
 
     let job = await getJob(context.params.id, prisma)
     job = JSON.parse(JSON.stringify(job))
 
+    // limit applications to just one, otherwise people will not remember 
+    // they already applied, and apply to the same job twice.
+    const applied = await alreadyApplied(
+        session.user.id,
+        context.params.id,
+        prisma
+    )
   return {
        props: {
             job,
-       }
+            applied,
+       },
      }
 }
