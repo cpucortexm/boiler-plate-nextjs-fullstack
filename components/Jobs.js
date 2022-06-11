4ab1148d6c589e7ad8e4a0e819f605d3a4565d55
Yogesh K <armsvcmode@gmail.com>
2022-06-11 23:13:41 +0530

    Show the applicants in the company dashboard

--- components/Jobs.js
+++ components/Jobs.js
@@ -2,13 +2,15 @@ import Job from 'components/Job'
 
 // jobs will be an array and we can iterate over using a map.
 // call the Job component to actually get the job details
-const Jobs = ({ jobs, isDashboard }) => {
+
+// remove the isDashboard prop from the Jobs component now, as we’ll not use that any more.
+const Jobs = ({ jobs }) => {
   if (!jobs) return null
 
   return (
     <>
       {jobs.map((job, index) => (
-        <Job key={index} job={job} isDashboard = {isDashboard}/>
+        <Job key={index} job={job} />
       ))}
     </>
   )
